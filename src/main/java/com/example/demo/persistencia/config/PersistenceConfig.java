e7b98e2de50d13e8fbaeee0d4fb952bdac105808
mlevis <mlevis@fepba.gov.ar>
2024-12-17 13:33:15 -0300

    cors

--- src/main/java/com/example/demo/persistencia/config/PersistenceConfig.java
+++ src/main/java/com/example/demo/persistencia/config/PersistenceConfig.java
@@ -19,23 +19,13 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+
 @Configuration // marca la clase como de configuracion
 @EnableTransactionManagement // habilita la anotación @Transactional
 @ComponentScan(basePackages="com.example.demo.persistencia")
 public class PersistenceConfig {
 	
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 
     @Bean
     public DataSource dataSource() {
@@ -70,6 +60,35 @@ public class PersistenceConfig {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(emf);
         return transactionManager;
+    }
+    
+ /*   @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }*/
+    
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*"); // Permite cualquier origen
+        config.addAllowedMethod("*");        // Permite cualquier método (GET, POST, etc.)
+        config.addAllowedHeader("*");        // Permite cualquier encabezado
+        config.setAllowCredentials(true);    // Permite el envío de cookies si es necesario
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Aplica a todos los endpoints
+
+        return new CorsFilter(source);
     }
     
 
